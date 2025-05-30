4f23332c67bf67085156eca87e24acab346997bb
Lucas W. Schuch <lucas.schuch@gmail.com>
2025-05-30 18:36:55 -0300

    Implement blog post creation and display features in the dashboard; add UI components for forms and cards

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((item) => (
-          <h1 key={item.title}>{item.title}</h1>
+          <h1 key={item.id}>{item.title}</h1>
         ))}
       </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "hobeirario.com.br",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
